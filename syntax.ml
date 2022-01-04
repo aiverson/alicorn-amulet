41ebf2bc16206fbe72d6686222e13cfb26cc896b
AstroSnail <astrosnail@protonmail.com>
2022-01-04 05:15:23 +0000

    Set up tests (not running yet)

--- syntax.ml
+++ syntax.ml
@@ -51,6 +51,7 @@ let alpha_lower = r "az"
 let alpha = alpha_upper `alt` alpha_lower
 let alnum = alpha `alt` num
 let alnum_ext = alnum `alt` p "_"
+let eof = neg star
 
 let keyword str = p str `seq` wsp
 let keysym str = p str `seq` wsq
@@ -82,9 +83,70 @@ let parser =
       let record_pair = collect_tuple (record_key `seq` keysym "=" `seq` term)
       in keysym "{" `seq` collect_list (sepseq record_pair (keysym ",")) `seq` keysym "}" `act` RecordCons `act` Fix
   (* TODO: is keysym "in" correct? *)
-  (* TODO: function bindings (probably need to adjust the term type) *)
+  (* TODO: rec bindings *)
+  (* TODO: function bindings *)
   , let_binding = keyword "let" `seq` collect_tuple (basic_id `seq` keysym "=" `seq` term `seq` keysym "in" `seq` term) `act` LetBinding `act` Fix
   , term = literal_bool `alt` (* string_cons `alt` *) list_cons `alt` record_cons `alt` identifier `alt` let_binding
   } term
 
-let foo = 0
+(* TESTS *)
+(* (test, expected result) pairs.
+ * Each section may be divided into 3 parts:
+ *   tests that should succeed;
+ *   tests that should fail;
+ *   tests that succeed but are questionable.
+ * In some cases the last part is meant to highlight ambiguities in Open's description
+ *)
+let parser_tests = [
+  (* Basic identifiers *)
+
+  ("list", Some (Identifier "list")),
+  ("sum", Some (Identifier "sum")),
+  ("foldl", Some (Identifier "foldl")),
+  ("this_is_a_s1lly_but_v4l1d_identifier", Some (Identifier "this_is_a_s1lly_but_v4l1d_identifier")),
+
+  ("_", None),
+  ("++", None),
+  ("_internal", None),
+
+  ("x___", Some (Identifier "x___")),
+  ("iCons", Some (Identifier "iCons")),
+  ("e", Some (Identifier "e")),
+
+  (* TODO: other tests (depends on the parsers) *)
+
+  (* Lists *)
+
+  ("[a,b,c]", Some (ListCons [Identifier "a", Identifier "b", Identifier "c"])),
+  ("[ a, b, c ]", Some (ListCons [Identifier "a", Identifier "b", Identifier "c"])),
+
+  (* Records *)
+
+  ("{a=b,c=d,e=f}", Some (RecordCons [(Identifier "a", Identifier "b"), (Identifier "c", Identifier "d"), (Identifier "e", Identifier "f")])),
+  ("{ a=b, c = d, e= f }", Some (RecordCons [(Identifier "a", Identifier "b"), (Identifier "c", Identifier "d"), (Identifier "e", Identifier "f")])),
+
+  (* Anonymous functions *)
+
+  (*("fun(a, b, c) = body", Some (ExprFunctionAnon (["a", "b", "c"], ExprId "body"))),*)
+
+  (* Function application *)
+
+  (*("foo(a, b, c)", Some (ExprFunctionCall ("foo", [ExprId "a", ExprId "b", ExprId "c"]))),*)
+
+  (* Let expressions *)
+  ("let name = expr in body", Some (LetBinding ("name", Identifier "expr", Identifier "body")))
+  (* don't forget the comma at the end of the previous line *)
+  (*("let rec name = expr in body", Some (ExprLet (LetRec, LetSimple ("name", ExprId "expr"), ExprId "body"))),*)
+  (*("let foo(a, b, c) = foocode in body", Some (ExprLet (Let, LetFunction ("foo", ["a", "b", "c"], ExprId "foocode"), ExprId "body")))*)
+]
+
+let parser_test parser (test, expected) =
+  let got = parse (parser `seq` eof) test
+  (expected == got, test, expected, got)
+let filter_failing results = filter (fun (success, _) -> not success) results
+let run_tests (parser, tests) =
+  let results = parser_test parser <$> tests
+  let fails = filter_failing results
+  map print fails
+(* TODO: impl show for term *)
+(*let _ = run_tests (parser, parser_tests)*)
